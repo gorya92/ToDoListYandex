8ac7e6a7988dfbdc1afeacb1b4bc5cc7dad6fd51
gorya92 <73346614+gorya92@users.noreply.github.com>
2022-08-15 19:10:37 +0300

    Delete LoadJsonResource.kt

--- app/src/androidTest/java/com/example/todoList/LoadJsonResource.kt
+++
@@ -1,8 +0,0 @@
-package com.example.todoList
-
-import okio.Okio
-
-fun String.toJsonString(): String {
-	val classLoader = Thread.currentThread().contextClassLoader
-	return classLoader.getResourceAsStream(this).use { `is` -> return Okio.buffer(Okio.source(`is`)).readUtf8() }
-}
