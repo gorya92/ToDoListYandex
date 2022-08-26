23717365a7771862b75a4e1cbb9889ac9d5cf9e3
gorya92 <pospielov.sasha.01@mail.ru>
2022-08-26 18:23:12 +0300

    Revert "product adding error fixed"
    This reverts commit f9992e55aab729847035af75e49514f990b8996e.

--- app/src/main/java/com/example/todolistyandex/model/TodoItemRepository.kt
+++ app/src/main/java/com/example/todolistyandex/model/TodoItemRepository.kt
@@ -4,4 +4,5 @@ import com.example.yandextask.model.TodoItem
 
 object TodoItemRepository {
     var todoList : ArrayList<TodoItem> = arrayListOf()
+    var todoListVisible : ArrayList<TodoItem> = arrayListOf()
 }
